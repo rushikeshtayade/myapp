053a2b757b420384be0852a7233e7d1af3ee6cb4
Rushikesh Tayade <rushikeshtayade201@gmail.com>
2020-12-23 09:41:47 +0530

    sencond commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+      
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +22,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
